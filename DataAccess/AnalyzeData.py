be68aa6b732acb9628cdbca9dfcf013b17c19564
Hasan Suhaimi <srip_arif@yahoo.com.my>
2019-01-04 12:19:43 +0000

    Update AnalyzeData.py

--- DataAccess/AnalyzeData.py
+++ DataAccess/AnalyzeData.py
@@ -1,7 +1,7 @@
 import yaml
 
 # open for reading
-with open("Dataset(sep29-aug30).txt", 'r') as dataset:
+with open("Dataset(oct18-oct17).txt", 'r') as dataset:
 
     # split every string ended with '\n' into a list of a string ['*']
     line_list = dataset.read().split('\n')
@@ -13,7 +13,23 @@ with open("Dataset(sep29-aug30).txt", 'r') as dataset:
             # convert stringed dict into dict "*" -> {*:**}
             line_dict = yaml.load(line)
             # accessing the key
-            print(line_dict['created_at'],"","retweet_count: ",line_dict['retweet_count'])
+            print(line_dict['created_at']," rtwt_count: ",
+                  line_dict['retweet_count']," fav_count: ",line_dict['favorite_count'],
+                  " quote_count: ",line_dict['quote_count']," reply_count: ",line_dict['reply_count'],
+                  " user_id: ",line_dict['user']["id"])
+
+            #check if key exist
+            if 'quoted_status' in line_dict:
+                print("Quoted from:", "{ " ,line_dict['quoted_status']['created_at'],"",
+                      " rtwt_count: ",line_dict['quoted_status']['retweet_count'],
+                      " fav_count: ",line_dict['quoted_status']['favorite_count'],
+                      " quote_count: ",line_dict['quoted_status']['quote_count'],
+                      " reply_count: ",line_dict['quoted_status']['reply_count'],
+                      " user_id:",line_dict['quoted_status']['user']["id"], " }")
+                print("")
+            else:
+                print("")
+
         else:
             break
 
